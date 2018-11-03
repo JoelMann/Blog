a3e90e43d486342367cb35eb9f0053bdadfe845a
JoelMann <jmann@ualberta.ca>
2018-11-02 20:25:24 -0800

    Server functioning, seed option input in comments, EJS views started.

--- app.js
+++ app.js
@@ -24,8 +24,27 @@ let blogSchema = new mongoose.Schema({
 
 let Blog = mongoose.model("Blog", blogSchema);
 
+// Blog.create({
+//     title: "Dylan is a faggot",
+//     image: "https://i.imgur.com/AHTuSs2.jpg",
+//     body: "ILL TELL YOU WHAT I WANT, WHAT I REALLY REALLY WANT"
+// });
+
 //Routes
 
+app.get('/', (req, res) => res.redirect('/blogs'));
+
+app.get('/blogs', (req, res) =>{
+    Blog.find({}, (err, blogs) => {
+        if(err){
+            console.log(err);
+            res.redirect('/404')
+        }
+        else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+})
 
 
 app.listen(3000, () => {
