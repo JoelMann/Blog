c730e4278d2817c28e3814e956e8067e06fb301d
JoelMann <jmann@ualberta.ca>
2018-11-02 19:15:23 -0700

    Staging and committing for other computer

--- app.js
+++ app.js
@@ -4,10 +4,29 @@ const express = require('express'),
     mongoose = require('mongoose');
 
 let app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 mongoose.connect('mongodb://localhost/blog', {
     useNewUrlParser: true
 });
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+let blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+let Blog = mongoose.model("Blog", blogSchema);
+
+//Routes
+
+
 
 app.listen(3000, () => {
     console.log("App is listening...")
