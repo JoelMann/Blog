2146627284b263f68726db703a3d587b7ad8c2fa
JoelMann <jmann@ualberta.ca>
2018-11-09 23:03:29 -0700

    Update route, Destroy route

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express'),
     cors = require('cors'), 
     bodyParser = require('body-parser'), 
     mongoose = require('mongoose'),
+    methodOveride = require('method-override');
     path = require('path');
 
 let app = express();
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(methodOveride('_method'));
 
 mongoose.connect('mongodb://localhost/blog', {
     useNewUrlParser: true
@@ -66,6 +68,36 @@ app.get('/blogs/:id', (req, res) => Blog.findById(req.params.id, (err, foundBlog
     }
 }
  ));
+
+ //Update
+ app.get('/blogs/:id/update', (req, res) => Blog.findById(req.params.id, (err, foundBlog) => {
+     if (err) {
+         res.redirect('/blogs:id')
+     }
+    res.render('update', {blog: foundBlog});
+ })
+);
+
+app.put('/blogs/:id', (req, res) => {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
+        if(err){
+            res.redirect('/blogs');
+        }
+        else {
+            res.redirect('/blogs/' + req.params.id);
+        }
+    });
+    console.log('Updated!')
+});
+
+app.delete('/blogs/:id/', (req, res) => Blog.findByIdAndRemove(req.params.id, (err, deletedBlog) => {
+    if (err) {
+        res.redirect('/blogs');
+    } else { 
+        console.log(deletedBlog);
+        res.redirect('/blogs');
+    }
+}))
 
 app.get('*', (req, res) => res.render('404'));
 app.listen(3000, () => console.log("App is listening..."));
